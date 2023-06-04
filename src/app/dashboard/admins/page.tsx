d1b6c4429ec9fe80e17a056302f43fcc938eeed5
Marcos Moraes <contato@marcosamoraes.com>
2023-06-03 21:47:59 -0300

    admins page added to sidebar

---
+++ src/app/dashboard/admins/page.tsx
@@ -0,0 +1,7 @@
+export default function Admins() {
+  return (
+    <>
+      <h1 className="w-full text-4xl text-gray-500 font-light mb-10">Administradores</h1>
+    </>
+  )
+}
--- src/components/Sidebar/Sidebar.tsx
+++ src/components/Sidebar/Sidebar.tsx
@@ -5,52 +5,78 @@ import { BiCategory } from 'react-icons/bi'
 import { BsBoxSeam } from 'react-icons/bs'
 import { FaSignOutAlt, FaTicketAlt } from 'react-icons/fa'
 import { FiSettings } from 'react-icons/fi'
+import { RiAdminFill } from 'react-icons/ri'
 
 export default function Sidebar() {
+
+  const pages = [
+    {
+      id: 'dashboard',
+      name: 'Dashboard',
+      icon: <AiOutlineHome size={24} />,
+      href: '/dashboard'
+    },
+    {
+      id: 'produtos',
+      name: 'Produtos',
+      icon: <BsBoxSeam size={24} />,
+      href: '/dashboard/produtos'
+    },
+    {
+      id: 'pedidos',
+      name: 'Pedidos',
+      icon: <AiOutlineShoppingCart size={24} />,
+      href: '/dashboard/pedidos'
+    },
+    {
+      id: 'clientes',
+      name: 'Clientes',
+      icon: <AiOutlineUser size={24} />,
+      href: '/dashboard/clientes'
+    },
+    {
+      id: 'categorias',
+      name: 'Categorias',
+      icon: <BiCategory size={24} />,
+      href: '/dashboard/categorias'
+    },
+    {
+      id: 'cupons',
+      name: 'Cupons',
+      icon: <FaTicketAlt size={24} />,
+      href: '/dashboard/cupons'
+    },
+    {
+      id: 'admins',
+      name: 'Administradores',
+      icon: <RiAdminFill size={24} />,
+      href: '/dashboard/admins'
+    },
+    {
+      id: 'configuracoes',
+      name: 'Configurações',
+      icon: <FiSettings size={24} />,
+      href: '/dashboard/configuracoes'
+    },
+    {
+      id: 'sair',
+      name: 'Sair',
+      icon: <FaSignOutAlt size={24} />,
+      href: '/dashboard/sair'
+    }
+  ]
   return (
     <aside className="h-full z-10 md:w-2/12 w-full -left-full md:left-0 absolute md:relative border-r-2 py-10 px-5 bg-zinc-800 text-white rounded-r-3xl flex flex-col flex-wrap items-start shadow-lg">
       <div className="w-full flex mb-10">
         <Image src="/logo-white.png" priority={true} alt="logo" width={300} height={300} />
       </div>
       <nav className="flex gap-2 flex-col w-full">
-        <Link className="w-full py-3 px-5 rounded-xl flex gap-3 items-center hover:bg-white/20 hover:shadow-md" href="/dashboard">
-          <AiOutlineHome size={24} />
-          Dashboard
-        </Link>
-        <Link className="w-full py-3 px-5 rounded-xl flex gap-3 items-center hover:bg-white/20 hover:shadow-md" href="/dashboard/produtos">
-          <BsBoxSeam size={24} />
-          Produtos
-        </Link>
-        <Link className="w-full py-3 px-5 rounded-xl flex gap-3 items-center hover:bg-white/20 hover:shadow-md" href="/dashboard/pedidos">
-          <AiOutlineShoppingCart size={24} />
-          Pedidos
-        </Link>
-        <Link className="w-full py-3 px-5 rounded-xl flex gap-3 items-center hover:bg-white/20 hover:shadow-md" href="/dashboard/clientes">
-          <AiOutlineUser size={24} />
-          Clientes
-        </Link>
-        <Link
-          className="w-full py-3 px-5 rounded-xl flex gap-3 items-center hover:bg-white/20 hover:shadow-md"
-          href="/dashboard/categorias"
-        >
-          <BiCategory size={24} />
-          Categorias
-        </Link>
-        <Link className="w-full py-3 px-5 rounded-xl flex gap-3 items-center hover:bg-white/20 hover:shadow-md" href="/dashboard/cupons">
-          <FaTicketAlt size={24} />
-          Cupons
-        </Link>
-        <Link
-          className="w-full py-3 px-5 rounded-xl flex gap-3 items-center hover:bg-white/20 hover:shadow-md"
-          href="/dashboard/configuracoes"
-        >
-          <FiSettings size={24} />
-          Configurações
-        </Link>
-        <Link className="w-full py-3 px-5 rounded-xl flex gap-3 items-center hover:bg-white/20 hover:shadow-md" href="/">
-          <FaSignOutAlt size={24} />
-          Sair
-        </Link>
+        {pages.map(page => (
+          <Link key={page.id} className="w-full py-3 px-5 rounded-xl flex gap-3 items-center hover:bg-white/20 hover:shadow-md" href={page.href}>
+            {page.icon}
+            {page.name}
+          </Link>
+        ))}
       </nav>
     </aside>
   )
